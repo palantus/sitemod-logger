2206a4d51018302f3ff4b9cf9d79f918a4b1cce7
Anders Kristiansen <palantus@gmail.com>
2023-10-31 16:49:02 +0100

    Use new api for stylesheets in components

--- www/pages/logger/setup.mjs
+++ www/pages/logger/setup.mjs
@@ -10,11 +10,10 @@ import "../../components/action-bar.mjs"
 import "../../components/action-bar-item.mjs"
 import {showDialog} from "../../components/dialog.mjs"
 import Toast from "../../components/toast.mjs"
+import { stylesheets } from "../../system/core.mjs"
 
 const template = document.createElement('template');
 template.innerHTML = `
-  <link rel='stylesheet' href='/css/global.css'>
-  <link rel='stylesheet' href='/css/searchresults.css'>
   <style>
     #container{
       padding: 10px;
@@ -105,7 +104,8 @@ class Element extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: 'open' })
+        .adoptedStyleSheets = [stylesheets.global, stylesheets.searchresults];
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
     this.refreshData = this.refreshData.bind(this);
